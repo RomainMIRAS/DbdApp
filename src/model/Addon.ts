ca3343552e500e6b74656d344ee26ef28f97dc05
RomainMIRAS <73794188+RomainMIRAS@users.noreply.github.com>
2024-06-11 18:46:27 +0200

    Fixing Add

--- src/model/Addon.ts
+++ src/model/Addon.ts
@@ -2,11 +2,11 @@
 import { Character } from './Character';
 
 export class Addon {
-    protected icon: string;
-    protected name: string;
-    protected description: string;
-    protected tier: number;
-    protected character: Character;
+    icon: string;
+    name: string;
+    description: string;
+    tier: number;
+    character: Character;
 
     constructor(icon: string, name: string, description: string, character: Character) {
         this.icon = icon;
